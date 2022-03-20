2f5e2755db363e75cadd009eab609056eb1c2cc1
rafaldsz <rd.szpecht@gmail.com>
2022-03-20 22:13:24 +0100

    Enviroment variables for TOPO_HOST and TOPO_PORT

--- planner/src/main/java/r2s2/planner/serviceClients/TopoServiceClient.java
+++ planner/src/main/java/r2s2/planner/serviceClients/TopoServiceClient.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.core.env.Environment;
 import r2s2.planner.plant.CityLandscape;
 
 
 @Controller
 public class TopoServiceClient {
 	
+    @Autowired
+    private Environment env;
+    
 	public String getTopographicDetail(String city) {
-		String URI = "http://toposervice:8080" + "/api/topographicdetails/";
+		String topoHost = env.getProperty("TOPO_HOST");
+		String topoPort = env.getProperty("TOPO_PORT");
+		
+		String URI = "http://" + topoHost + ":" + topoPort + "/api/topographicdetails/";
 		
 		RestTemplate restTemplate = new RestTemplate();
 		String url = URI + city;
